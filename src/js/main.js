1862eef8a7e6ae027f6c557d7ca12a2d6cc166be
alex.fan <alex.fan@okchem.com>
2018-07-03 16:19:33 +0800

    refactor code to use throttle in place of debounce

--- src/js/main.js
+++ src/js/main.js
@@ -31,7 +31,7 @@ $(function () {
         var scrollTop = $(window).scrollTop(),
             height = $(window).height(),
             docHeight = $(document).height()
-        console.log(scrollTop + ' ' +  height + ' ' + docHeight)
+        console.log('window.scrollTop():' + scrollTop + ' window.height():' +  height + ' document.height():' + docHeight)
         return scrollTop + height === docHeight
     }
 
@@ -48,12 +48,20 @@ $(function () {
     loadMoreItems()
 
     // $(window).on('scroll', function () {
-    //     console.log(isPageBottomReached())
+    //     if (isPageBottomReached()) {
+    //         loadMoreItems()
+    //     }
     // })
 
-    $(window).on('scroll', _.debounce(function () {
+    // $(window).on('scroll', _.debounce(function () {
+    //     if (isPageBottomReached()) {
+    //         loadMoreItems()
+    //     }
+    // }, 200))
+
+    $(window).on('scroll', _.throttle(function () {
         if (isPageBottomReached()) {
             loadMoreItems()
         }
-    }, 200))
+    }, 300))
 })
